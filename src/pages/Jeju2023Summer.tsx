4e7958e08221d3c5205c42200276ebab0f643a42
idonghoon <devarchi33@naver.com>
2023-07-10 15:29:47 +0900

    본문영역 추가

--- src/pages/Jeju2023Summer.tsx
+++ src/pages/Jeju2023Summer.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import {Table} from "antd";
+import plans from '../datasources/jeju.json';
 
 import "./Jeju2023Summer.scss"
 
 function Jeju2023Summer() {
+    const parsedPlans = plans.map((item: any, idx: number) => {
+        return {...item, no: idx + 1}
+    })
     return (
         <div id="content">
             <header id="header" className="App-header">
                 <h1>제주여행 2023</h1>
             </header>
-            <section id="main"></section>
+            <section id="main">
+                <Table
+                    bordered={true}
+                    pagination={false}
+                    columns={[
+                        {
+                            key: "day",
+                            dataIndex: "day",
+                            title: "순서",
+                            align: "center"
+                        },
+                        {
+                            key: "date",
+                            dataIndex: "date",
+                            title: "날짜",
+                            align: "center"
+                        },
+                        {
+                            key: "day of  the week",
+                            dataIndex: "day of  the week",
+                            title: "요일",
+                            align: "center"
+                        }
+                    ]}
+                    dataSource={parsedPlans}
+                />
+            </section>
         </div>
     )
 }
